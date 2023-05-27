c14580d6c3b61a38f48a908bdb159d8eccb1f36a
calm1205 <naginani072@gmail.com>
2023-05-27 15:15:50 +0900

    [feat] insert comment

--- src/minimum/Minimum.tsx
+++ src/minimum/Minimum.tsx
@@ -7,33 +7,33 @@ import { Item } from "../components/Item";
  */
 export const Minimum = () => {
   // ref
-  const wrapperRef = useRef(null);
-  const itemRef = useRef(null);
+  const observeAreaRef = useRef(null); // 監視対象の範囲
+  const observeTargetRef = useRef(null); // 監視されるDom
 
   const callback = () => console.log("scroll bottom.");
 
   useEffect(() => {
-    if (!wrapperRef.current) return;
-    if (!itemRef.current) return;
+    if (!observeAreaRef.current) return;
+    if (!observeTargetRef.current) return;
 
     const observer = new IntersectionObserver(callback, {
-      root: wrapperRef.current,
-      threshold: 0.5,
+      root: observeAreaRef.current,
+      threshold: 0.5, // 監視対象の範囲に監視対象が <threshold>%見えるようになったらcallbackを発火
     });
-    observer.observe(itemRef.current);
+    observer.observe(observeTargetRef.current);
   }, []);
 
   return (
     <div>
       <h2>最小構成</h2>
 
-      <Wrapper ref={wrapperRef}>
+      <Wrapper ref={observeAreaRef}>
         <Item index={0} />
         <Item index={1} />
         <Item index={2} />
         <Item index={3} />
         <Item index={4} />
-        <Item index={5} ref={itemRef} />
+        <Item index={5} ref={observeTargetRef} />
       </Wrapper>
     </div>
   );
