5e2f56300c198464da3ab58254ca2a80a7b82386
calm1205 <naginani072@gmail.com>
2023-05-27 15:33:20 +0900

    [feat] sample api call

--- src/apiCall/ApiCall.tsx
+++ src/apiCall/ApiCall.tsx
@@ -1,7 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+
 /**
  * APIコールを含む無限スクロール
  */
 export const ApiCall = () => {
+  const queryFn = async () =>
+    (await fetch("https://api.sampleapis.com/beers/ale")).json();
+
+  const { data } = useQuery({ queryKey: ["sampleQuery"], queryFn });
+  console.log(data);
+
   return (
     <div>
       <h2>API呼び出し</h2>
